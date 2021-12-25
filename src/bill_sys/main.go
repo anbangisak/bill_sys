a973592b5994240b0712b7296becd76e259137d3
anbangisak <anbangisak@gmail.com>
2021-12-25 23:30:32 +0530

    simple server started

--- src/bill_sys/main.go
+++ src/bill_sys/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main(){
-    fmt.Println("Hello World!")
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world from GfG")
+	})
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+	port := ":8200"
+	fmt.Println("Server is running on port" + port)
+
+	// Start server on port specified above
+	log.Fatal(http.ListenAndServe(port, nil))
+	fmt.Println("Hello World!")
 }
