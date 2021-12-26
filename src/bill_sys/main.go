fb56237c02b1670a74a81f30829df2d65b7d463d
anbangisak <anbangisak@gmail.com>
2021-12-26 11:02:47 +0530

    db connection added yet to verify queries

--- src/bill_sys/main.go
+++ src/bill_sys/main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type person struct {
+	id         int
+	first_name string
+	last_name  string
+	email      string
+	ip_address string
+}
+
+func addPerson(db *sql.DB, newPerson person) {
+	stmt, _ := db.Prepare("INSERT INTO people (id, first_name, last_name, email, ip_address) VALUES (?, ?, ?, ?, ?)")
+	stmt.Exec(nil, newPerson.first_name, newPerson.last_name, newPerson.email, newPerson.ip_address)
+	defer stmt.Close()
+
+	fmt.Printf("Added %v %v \n", newPerson.first_name, newPerson.last_name)
+}
+
+func prepareAddPerson(db *sql.DB) {
+	newPerson := person{
+		first_name: "anban",
+		last_name:  "gisak",
+		email:      "anbangisak@gmail.com",
+		ip_address: "127.0.0.1",
+	}
+
+	addPerson(db, newPerson)
+}
+
 func main() {
+	// db, err := sql.Open("sqlite3", "./auro.db")
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// prepareAddPerson(db)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world from GfG")
 	})
