94d6b8e219f3e63f57c59d4879a146a11f9bd298
anbangisak <anbangisak@gmail.com>
2021-12-31 02:46:10 +0530

    pdf half ready - table is pending

--- src/bill_sys/main.go
+++ src/bill_sys/main.go
@@ -7,8 +7,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/jung-kurt/gofpdf"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/sony/sonyflake"
 )
 
 type person struct {
@@ -17,6 +20,17 @@ type person struct {
 	last_name  string
 	email      string
 	ip_address string
+}
+
+func genSonyflake() uint64 {
+	flake := sonyflake.NewSonyflake(sonyflake.Settings{})
+	id, err := flake.NextID()
+	if err != nil {
+		log.Fatalf("flake.NextID() failed with %s\n", err)
+	}
+	// Note: this is base16, could shorten by encoding as base62 string
+	fmt.Printf("github.com/sony/sonyflake:      %x\n", id)
+	return id
 }
 
 type dashboard struct {
@@ -73,6 +87,88 @@ func createInsertDefaultUser(db *sql.DB) {
 	tx.Commit()
 }
 
+/*
+func tableMulti(pdf *gofpdf.Fpdf, cols []float64, rows [][]string) {
+    pagew, pageh := pdf.GetPageSize()
+    _ = pagew
+    mleft, mright, mtop, mbottom := pdf.GetMargins()
+    _ = mleft
+    _ = mright
+    _ = mtop
+
+    for _, row := range rows {
+        curx, y := pdf.GetXY()
+        x := curx
+
+        height := 0.
+        _, lineHt := pdf.GetFontSize()
+
+        for i, txt := range row {
+            lines := pdf.SplitLines([]byte(txt), cols[i])
+            h := float64(len(lines))*lineHt + float64(MARGECELL*len(lines))
+            if h > height {
+                height = h
+            }
+        }
+        // add a new page if the height of the row doesn't fit on the page
+        if pdf.GetY()+height > pageh-mbottom {
+            pdf.AddPage()
+            y = pdf.GetY()
+        }
+        for i, txt := range row {
+            width := cols[i]
+            pdf.Rect(x, y, width, height, "")
+            pdf.MultiCell(width, lineHt+MARGECELL, txt, "", "", false)
+            x += width
+            pdf.SetXY(x, y)
+        }
+        pdf.SetXY(curx, y+height)
+    }
+}
+*/
+func GeneratePdf(filename string) error {
+
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+
+	// ImageOptions(src, x, y, width, height, flow, options, link, linkStr)
+	pdf.ImageOptions(
+		"logo.png",
+		85, 5,
+		40, 30,
+		false,
+		gofpdf.ImageOptions{ImageType: "PNG", ReadDpi: true},
+		0,
+		"",
+	)
+
+	pdf.SetFont("Arial", "B", 28)
+	pdf.SetY(40)
+	// CellFormat(width, height, text, border, position after, align, fill, link, linkStr)
+	pdf.CellFormat(190, 7, "KINGS SYSTEMS", "0", 2, "CM", false, 0, "")
+
+	pdf.SetFont("Arial", "", 16)
+	pdf.SetY(50)
+	pdf.CellFormat(190, 7, "(ONLINE e-TDS/TCS SOLUTIONS)", "0", 2, "CM", false, 0, "")
+
+	pdf.SetFont("Arial", "BU", 18)
+	pdf.SetY(70)
+	pdf.CellFormat(190, 7, "INVOICE", "0", 2, "CM", false, 0, "")
+
+	pdf.SetFont("Arial", "", 12)
+	pdf.SetXY(0, 80)
+	invoice_num := genSonyflake()
+	invoice_num_str := fmt.Sprintf("INVOICE NO: %x", invoice_num)
+	pdf.CellFormat(100, 7, invoice_num_str, "0", 0, "CM", false, 0, "")
+
+	pdf.SetXY(135, 80)
+	date_val := time.Now()
+	date_str := fmt.Sprintf("Date: %v", date_val.Format("02/01/2006"))
+	pdf.CellFormat(80, 7, date_str, "0", 0, "CM", false, 0, "")
+
+	return pdf.OutputFileAndClose(filename)
+}
+
 func main() {
 	db, err := sql.Open("sqlite3", "./auro.db")
 	// db, err := sql.Open("sqlite3", ":memory:")
@@ -88,6 +184,11 @@ func main() {
 	rows, err := db.Query("select * from USER")
 	checkErr(err)
 	defer rows.Close()
+
+	err = GeneratePdf("hello.pdf")
+	if err != nil {
+		panic(err)
+	}
 
 	dashboard_data := dashboard{
 		Title:      "Kings Systems",
---
+++ src/bill_sys/readme.md
@@ -0,0 +1,9 @@
+go mod init bill_sys
+go mod tidy
+
+gcc install:
+https://medium.com/@yaravind/go-sqlite-on-windows-f91ef2dacfe
+
+gcc path set:
+http://www.codebind.com/cprogramming/install-mingw-windows-10-gcc/
+
