e2063c4e7d77d2c911bfab151193bba6832ceb00
anbangisak <anbangisak@gmail.com>
2022-01-02 03:59:53 +0530

    pdf draw completed - pending export, crud

--- src/bill_sys/main.go
+++ src/bill_sys/main.go
@@ -87,47 +87,14 @@ func createInsertDefaultUser(db *sql.DB) {
 	tx.Commit()
 }
 
-/*
-func tableMulti(pdf *gofpdf.Fpdf, cols []float64, rows [][]string) {
-    pagew, pageh := pdf.GetPageSize()
-    _ = pagew
-    mleft, mright, mtop, mbottom := pdf.GetMargins()
-    _ = mleft
-    _ = mright
-    _ = mtop
-
-    for _, row := range rows {
-        curx, y := pdf.GetXY()
-        x := curx
-
-        height := 0.
-        _, lineHt := pdf.GetFontSize()
-
-        for i, txt := range row {
-            lines := pdf.SplitLines([]byte(txt), cols[i])
-            h := float64(len(lines))*lineHt + float64(MARGECELL*len(lines))
-            if h > height {
-                height = h
-            }
-        }
-        // add a new page if the height of the row doesn't fit on the page
-        if pdf.GetY()+height > pageh-mbottom {
-            pdf.AddPage()
-            y = pdf.GetY()
-        }
-        for i, txt := range row {
-            width := cols[i]
-            pdf.Rect(x, y, width, height, "")
-            pdf.MultiCell(width, lineHt+MARGECELL, txt, "", "", false)
-            x += width
-            pdf.SetXY(x, y)
-        }
-        pdf.SetXY(curx, y+height)
-    }
-}
-*/
 func GeneratePdf(filename string) error {
 
+	// const (
+	// 	colCount = 4
+	// 	rowCount = 3
+	// 	margin   = 32.0
+	// 	fontHt   = 14.0 // point
+	// )
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 
@@ -152,24 +119,124 @@ func GeneratePdf(filename string) error {
 	pdf.CellFormat(190, 7, "(ONLINE e-TDS/TCS SOLUTIONS)", "0", 2, "CM", false, 0, "")
 
 	pdf.SetFont("Arial", "BU", 18)
-	pdf.SetY(70)
+	pdf.SetY(60)
 	pdf.CellFormat(190, 7, "INVOICE", "0", 2, "CM", false, 0, "")
 
 	pdf.SetFont("Arial", "", 12)
-	pdf.SetXY(0, 80)
+	pdf.SetXY(0, 70)
 	invoice_num := genSonyflake()
 	invoice_num_str := fmt.Sprintf("INVOICE NO: %x", invoice_num)
 	pdf.CellFormat(100, 7, invoice_num_str, "0", 0, "CM", false, 0, "")
 
-	pdf.SetXY(135, 80)
+	pdf.SetXY(135, 70)
 	date_val := time.Now()
 	date_str := fmt.Sprintf("Date: %v", date_val.Format("02/01/2006"))
 	pdf.CellFormat(80, 7, date_str, "0", 0, "CM", false, 0, "")
+
+	// tanOffiName table
+	pdf.SetFont("Arial", "B", 11)
+	pdf.SetXY(20, 80)
+	pdf.CellFormat(30.0, 10.0, "TAN NO", "1", 0, "LM", false, 0, "")
+	tanNo := "CHET18709F"
+	pdf.SetFont("Arial", "", 11)
+	pdf.SetXY(50, 80)
+	pdf.CellFormat(88.0, 10.0, tanNo, "1", 0, "LM", false, 0, "")
+	pdf.SetFont("Arial", "B", 11)
+	pdf.SetXY(20, 90)
+	pdf.CellFormat(30.0, 24.0, "OFFICE NAME", "1", 0, "LM", false, 0, "")
+	officeName := "PHC MUGAIYUR."
+	pdf.SetFont("Arial", "", 11)
+	pdf.SetXY(50, 90)
+	pdf.CellFormat(88.0, 24.0, officeName, "1", 0, "LM", false, 0, "")
+
+	// desc & amount table
+	pdf.SetFont("Arial", "", 11)
+	pdf.SetXY(20, 124)
+	pdf.CellFormat(110.0, 14.0, "DESCRIPTION", "1", 0, "CM", false, 0, "")
+
+	pdf.SetXY(130, 124)
+	pdf.CellFormat(30.0, 14.0, "AMOUNT", "1", 0, "LM", false, 0, "")
+
+	ht := pdf.PointConvert(8)
+	fyStr := "2021-2022"
+
+	desc1 := "Fees for e-Filing Income Tax Quarterly returns"
+	desc2 := "(Tax Deducted at Source)for the FY " + fyStr + " and"
+	desc3 := "Quarters Q1,Q2,Q3 and Q4"
+
+	pdf.SetXY(20, 138)
+	pdf.CellFormat(110.0, 24.0, "", "1", 1, "LM", false, 0, "")
+	pdf.SetXY(28, 143)
+	pdf.CellFormat(110.0, ht, desc1, "", 1, "LM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(28, 151)
+	pdf.CellFormat(110.0, ht, desc2, "", 1, "LM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(28, 159)
+	pdf.CellFormat(110.0, ht, desc3, "", 1, "LM", false, 0, "")
+	pdf.Ln(ht)
+	amount := 1400.00
+	pdf.SetXY(130, 138)
+	pdf.CellFormat(30.0, 24.0, fmt.Sprintf("%v", amount), "1", 0, "LM", false, 0, "")
+
+	pdf.SetXY(20, 162)
+	pdf.CellFormat(110.0, 20.0, "TOTAL BILL AMOUNT", "1", 0, "RM", false, 0, "")
+
+	pdf.SetXY(130, 162)
+	pdf.CellFormat(30.0, 20.0, fmt.Sprintf("%v", amount), "1", 0, "LM", false, 0, "")
+
+	pdf.SetXY(15, 182)
+	// date_val := time.Now()
+	rupeesStr := "(Rupees One thousand and Four Hundred Only)"
+	pdf.CellFormat(110, 10, rupeesStr, "0", 0, "CM", false, 0, "")
+
+	pdf.SetXY(20, 202)
+	pdf.CellFormat(110.0, 40.0, "", "", 1, "LM", false, 0, "")
+	pdf.SetXY(28, 202)
+	pdf.CellFormat(110.0, ht, "Remarks", "", 1, "LM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(28, 210)
+	pdf.CellFormat(110.0, ht, "Kindly make payments to,", "", 1, "LM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(28, 218)
+	pdf.CellFormat(110.0, ht, "Name: J.Himmam Hussain", "", 1, "LM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(28, 224)
+	pdf.CellFormat(110.0, ht, "Account No: 2697101023500", "", 1, "LM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(28, 232)
+	pdf.CellFormat(110.0, ht, "IFSC Code : CNRB0002697", "", 1, "LM", false, 0, "")
+
+	pdf.SetXY(135, 232)
+	pdf.CellFormat(70.0, 24.0, "", "", 1, "LM", false, 0, "")
+	pdf.SetXY(135, 232)
+	pdf.CellFormat(70.0, ht, "(J.Himmam Hussain)", "", 1, "CM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(135, 240)
+	pdf.CellFormat(70.0, ht, "FOR KINGS SYSTEMS,", "", 1, "CM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(135, 248)
+	pdf.CellFormat(70.0, ht, "VILLUPURAM", "", 1, "CM", false, 0, "")
+
+	pdf.Line(0, 253, 250, 253)
+
+	ht = pdf.PointConvert(6)
+	pdf.SetXY(35, 258)
+	pdf.CellFormat(150.0, 18.0, "", "", 1, "LM", false, 0, "")
+	pdf.SetXY(35, 258)
+	pdf.CellFormat(150.0, ht, "Shop No: 14, Quber plaza, Opp. to Collectorate complex, Villupuram - 605602.", "", 1, "CM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(35, 264)
+	pdf.CellFormat(150.0, ht, "Mobile : 9994471707, Landline : 04146 - 227707, 04146 - 356394", "", 1, "CM", false, 0, "")
+	pdf.Ln(ht)
+	pdf.SetXY(35, 270)
+	pdf.CellFormat(150.0, ht, "E-mail : kingsetdscenter@gmail.com", "", 1, "CM", false, 0, "")
 
 	return pdf.OutputFileAndClose(filename)
 }
 
 func main() {
+
 	db, err := sql.Open("sqlite3", "./auro.db")
 	// db, err := sql.Open("sqlite3", ":memory:")
 	checkErr(err)
